8127fabd606dd24507f0554ea31fa139a6aa81e4
MihailsKrasikovs <mihails.krasikovs@ideaportriga.lv>
2021-10-13 12:43:59 +0300

    grpc interface

--- shopping-cart-service/src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
+++ shopping-cart-service/src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
@@ -5,7 +5,7 @@ import akka.grpc.GrpcServiceException
 import akka.util.Timeout
 import io.grpc.Status
 import org.slf4j.LoggerFactory
-import shopping.cart.proto.{AddItemRequest, Cart}
+import shopping.cart.proto.{AddItemRequest, Cart, CheckoutRequest, RemoveItemRequest}
 
 import scala.concurrent.{ExecutionContext, Future, TimeoutException}
 
@@ -20,11 +20,7 @@ class ShoppingCartServiceImpl(system: ActorSystem[_]) extends proto.ShoppingCart
   private val sharding = ClusterSharding(system)
 
   override def addItem(in: AddItemRequest): Future[Cart] = {
-    logger.info(
-      "addItem {} with quantity {} to cart {}",
-      in.itemId,
-      in.quantity,
-      in.cartId)
+    logger.info("addItem {} with quantity {} to cart {}", in.itemId, in.quantity, in.cartId)
 //    Future.successful(Cart(items = List(Item(in.itemId, in.quantity))))
     val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
     val reply: Future[ShoppingCart.Summary] = entityRef.askWithStatus(ShoppingCart.AddItem(in.itemId, in.quantity, _))
@@ -32,10 +28,44 @@ class ShoppingCartServiceImpl(system: ActorSystem[_]) extends proto.ShoppingCart
     convertError(response)
   }
 
+  override def removeItem(in: RemoveItemRequest): Future[Cart] = {
+    logger.info("removeItem {} to cart {}", in.itemId, in.cartId)
+
+    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
+    val reply: Future[ShoppingCart.Summary] = entityRef.askWithStatus(ShoppingCart.RemoveItem(in.itemId, _))
+    val response = reply.map(cart => toProtoCart(cart))
+    convertError(response)
+  }
+
+  override def checkout(in: CheckoutRequest): Future[Cart] = {
+    logger.info("checkout cart {}", in.cartId)
+
+    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
+//    val reply: Future[ShoppingCart.Summary] = entityRef.askWithStatus(ShoppingCart.Checkout(_))
+    val reply: Future[ShoppingCart.Summary] = entityRef.askWithStatus(ShoppingCart.Checkout)
+    val response = reply.map(cart => toProtoCart(cart))
+    convertError(response)
+  }
+
+  override def getCart(in: proto.GetCartRequest): Future[proto.Cart] = {
+    logger.info("getCart {}", in.cartId)
+    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
+    val response =
+      entityRef.ask(ShoppingCart.Get).map { cart =>
+        if (cart.items.isEmpty)
+          throw new GrpcServiceException(Status.NOT_FOUND.withDescription(s"Cart ${in.cartId} not found"))
+        else
+          toProtoCart(cart)
+      }
+    convertError(response)
+  }
+
   private def toProtoCart(cart: ShoppingCart.Summary): proto.Cart = {
-    proto.Cart(cart.items.iterator.map { case (itemId, quantity) =>
-      proto.Item(itemId, quantity)
-    }.toSeq)
+    proto.Cart(
+      cart.items.iterator.map { case (itemId, quantity) =>
+        proto.Item(itemId, quantity)
+      }.toSeq,
+      cart.checkedOut)
   }
 
   private def convertError[T](response: Future[T]): Future[T] = {
