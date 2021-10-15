af9f93d0dfb386282d7e60778c64bcbcc3d0d722
MihailsKrasikovs <mihails.krasikovs@ideaportriga.lv>
2021-10-15 09:04:38 +0300

    add SupervisingActor

---
+++ akka-quickstart-scala/src/main/scala/com/example/SupervisingActor.scala
@@ -0,0 +1,69 @@
+package com.example
+
+import akka.actor.typed.{ActorSystem, Behavior, PostStop, PreRestart, Signal, SupervisorStrategy}
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext}
+import akka.actor.typed.scaladsl.Behaviors
+
+object SupervisingActor {
+
+  def apply(): Behavior[String] = Behaviors.setup(context => new SupervisingActor(context))
+
+  class SupervisingActor(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+    private val child = context.spawn(
+      Behaviors.supervise(SupervisedActor()).onFailure(SupervisorStrategy.restart),
+      name = "supervised-actor")
+
+    override def onMessage(msg: String): Behavior[String] =
+      msg match {
+        case "failChild" =>
+          child ! "fail"
+          this
+      }
+  }
+
+}
+
+object SupervisedActor {
+  def apply(): Behavior[String] = Behaviors.setup(context => new SupervisedActor(context))
+}
+
+class SupervisedActor(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  println("supervised actor started")
+
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "fail" =>
+        println("supervised actor fails now")
+        throw new Exception("I failed!")
+    }
+
+  override def onSignal: PartialFunction[Signal, Behavior[String]] = {
+    case PreRestart =>
+      println("supervised actor will be restarted")
+      this
+    case PostStop =>
+      println("supervised actor stopped")
+      this
+  }
+
+}
+
+object Main {
+  def apply(): Behavior[String] = Behaviors.setup(context => new Main(context))
+}
+
+class Main(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  override def onMessage(msg: String): Behavior[String] = {
+    msg match {
+      case "start" =>
+        val supervisingActor = context.spawn(SupervisingActor(), "supervising-actor")
+        supervisingActor ! "failChild"
+        this
+    }
+  }
+}
+
+object Experiments extends App {
+  val testSystem = ActorSystem(Main(), "testSystem")
+  testSystem ! "start"
+}
