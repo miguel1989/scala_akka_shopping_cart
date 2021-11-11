fc133560c9b65d97e39e7fb1e112431823f751d6
mihailsk <Mihails.krasikovs@Onairentertainment.com>
2021-11-11 10:55:51 +0200

    training classes

--- scala-at-light-speed/src/main/scala/com/rockthejvm/udemy/advanced/part2/MySet.scala
+++ scala-at-light-speed/src/main/scala/com/rockthejvm/udemy/advanced/part2/MySet.scala
@@ -8,11 +8,17 @@ trait MySet[A] extends (A => Boolean){
   def contains(elem: A): Boolean
   def +(elem: A): MySet[A]
   def ++(set: MySet[A]): MySet[A]
+  def -(elem: A): MySet[A]
+
+  def --(set: MySet[A]): MySet[A]
+  def &(set: MySet[A]): MySet[A]
 
   def map[B](f:A => B): MySet[B]
   def flatMap[B](f:A => MySet[B]): MySet[B]
   def filter(f: A => Boolean): MySet[A]
   def foreach(f: A=> Unit): Unit
+
+  def unary_! : MySet[A]
 }
 
 class MyEmptySet[A] extends MySet[A] {
@@ -22,6 +28,11 @@ class MyEmptySet[A] extends MySet[A] {
 
   override def ++(set: MySet[A]): MySet[A] = set
 
+  def -(elem: A): MySet[A] = this
+
+  def --(set: MySet[A]): MySet[A] = this
+  def &(set: MySet[A]): MySet[A] = this
+
   override def map[B](f: A => B): MySet[B] = new MyEmptySet[B]
 
   override def flatMap[B](f: A => MySet[B]): MySet[B] = new MyEmptySet[B]
@@ -29,6 +40,25 @@ class MyEmptySet[A] extends MySet[A] {
   override def filter(f: A => Boolean): MySet[A] = this
 
   override def foreach(f: A => Unit): Unit = ()
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](_ => true)
+}
+
+//all elements of type A which satisfy prop
+class PropertyBasedSet[A](prop: A => Boolean) extends MySet[A] {
+  override def contains(elem: A): Boolean = prop(elem)
+  override def +(elem: A): MySet[A] = new PropertyBasedSet[A](x => prop(x) || x == elem)
+  override def ++(set: MySet[A]): MySet[A] = new PropertyBasedSet[A](x => prop(x) || set(x))
+  override def -(elem: A): MySet[A] = filter(x => x != elem)
+  override def --(set: MySet[A]): MySet[A] = filter(!set)
+  override def &(set: MySet[A]): MySet[A] = filter(set)
+  override def map[B](f: A => B): MySet[B] = politeFail
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = politeFail
+  override def filter(f: A => Boolean): MySet[A] = new PropertyBasedSet[A](x => prop(x) && f(x))
+  override def foreach(f: A => Unit): Unit = politeFail
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](x => !prop(x))
+
+  def politeFail = throw new RuntimeException("deep rabbit hole")
 }
 
 class NonEmptySet[A](val head: A, tail: MySet[A]) extends MySet[A] {
@@ -40,6 +70,16 @@ class NonEmptySet[A](val head: A, tail: MySet[A]) extends MySet[A] {
   }
 
   override def ++(set: MySet[A]): MySet[A] = tail ++ set + head
+
+  def -(elem: A): MySet[A] = {
+    if (head == elem) tail
+    else tail - elem + head
+  }
+
+  def --(set: MySet[A]): MySet[A] = filter(x => !set.contains(x))
+
+//  def &(set: MySet[A]): MySet[A] = filter(x => set.contains(x)) //or set(x)
+  def &(set: MySet[A]): MySet[A] = filter(set)
 
   override def map[B](f: A => B): MySet[B] = tail.map(f) + f(head)
 
@@ -55,6 +95,8 @@ class NonEmptySet[A](val head: A, tail: MySet[A]) extends MySet[A] {
     f(head)
     tail.foreach(f)
   }
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](x => !this.contains(x))
 }
 
 object MySet {
@@ -71,6 +113,9 @@ object MySet {
 
 object test extends App {
   val mySet = new NonEmptySet[Int](3, new MyEmptySet[Int])
+  val mySet2 = mySet + 7
 
-  mySet.map(x => x * 4).foreach(println)
+//  mySet.map(x => x * 4).foreach(println)
+  mySet2.foreach(println)
+  (mySet2 - 2).foreach(println)
 }
