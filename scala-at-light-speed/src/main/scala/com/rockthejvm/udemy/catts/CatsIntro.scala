321a89dc20b9210fa75c2a91176f3a3f907bc0c8
mihailsk <Mihails.krasikovs@Onairentertainment.com>
2021-11-19 08:58:20 +0200

    training materials

--- scala-at-light-speed/src/main/scala/com/rockthejvm/udemy/catts/CatsIntro.scala
+++ scala-at-light-speed/src/main/scala/com/rockthejvm/udemy/catts/CatsIntro.scala
@@ -1,4 +1,4 @@
-package com.rockthejvm.udemy
+package com.rockthejvm.udemy.catts
 
 import cats.effect.IO
 
@@ -16,7 +16,9 @@ object CatsIntro extends App {
   val compare = 1 === 3
 
   //extending TC operations to composite types
+
   import cats.instances.list._ //we bring Eq[List[Int]]
+
   val aListComparison = List(1) === List(2)
 
   case class ToyCar(model: String, price: Double)
@@ -26,24 +28,25 @@ object CatsIntro extends App {
 
   //--------------------------------------------------------------------------------------------------
   //FUNCTORS
-  import cats.Functor
-  import cats.Monad
+
+  import cats.{Functor, Monad}
 
   def do10xWithFunctor[F[_]](container: F[Int])(implicit functor: Functor[F]): F[Int] =
     functor.map(container)(x => x * 10)
 
-  println(do10xWithFunctor(List(1,2,3)))
+  println(do10xWithFunctor(List(1, 2, 3)))
   println(do10xWithFunctor(Option(12)))
 
- import cats.syntax.flatMap._
- import cats.syntax.functor._
-  def getPairsMonads[M[_], A, B](ma: M[A], mb: M[B])(implicit monad: Monad[M]): M[(A,B)] =
+  import cats.syntax.flatMap._
+  import cats.syntax.functor._
+
+  def getPairsMonads[M[_], A, B](ma: M[A], mb: M[B])(implicit monad: Monad[M]): M[(A, B)] =
     for {
       a <- ma
       b <- mb
-    } yield (a,b)
+    } yield (a, b)
 
-  println(getPairsMonads(List(1,2,3), List('a','b','c')))
+  println(getPairsMonads(List(1, 2, 3), List('a', 'b', 'c')))
   println(getPairsMonads(Option(42), Option('x')))
   //--------------------------------------------------------------------------------------------------
   //Applicatives = Functor + pure
@@ -58,31 +61,36 @@ object CatsIntro extends App {
   val anOpt = optionAppl.pure(3) //Some(2)
 
   import cats.syntax.applicative._
+
   val aSweetList = 2.pure[List]
   val aSweetOption = 2.pure[Option]
 
   import cats.data.Validated
+
   type ErrorsOr[T] = Validated[List[String], T]
   val aValidVal: ErrorsOr[Int] = Validated.valid(43)
   val aModifiedValidated = aValidVal.map(_ + 1)
   val validatedApplicative = Applicative[ErrorsOr]
 
-//  def ap[W[_], B, T](wf: W[B => T])(wa: W[B]): W[T] = ???
+  //  def ap[W[_], B, T](wf: W[B => T])(wa: W[B]): W[T] = ???
   def productWithApplicatives[W[_], A, B](wa: W[A], wb: W[B])(implicit applicative: Applicative[W]): W[(A, B)] = {
-    val funcWrap: W[B => (A, B)] = applicative.map(wa)(a => (b: B) => (a,b))
+    val funcWrap: W[B => (A, B)] = applicative.map(wa)(a => (b: B) => (a, b))
     applicative.ap(funcWrap)(wb)
   }
 
-  println(productWithApplicatives(List(1,2,3), List('a', 'b')))
+  println(productWithApplicatives(List(1, 2, 3), List('a', 'b')))
 
 
+  val listOfOptions: List[Option[Int]] = List(Some(1), Some(2))
 
-  val listOfOptions: List[Option[Int]] =  List(Some(1), Some(2))
   import cats.Traverse
+
   val listTraverse = Traverse[List]
-  val optionListInt:Option[List[Int]] = listTraverse.traverse(List(1,2,3))(x => Option(x))
+  val optionListInt: Option[List[Int]] = listTraverse.traverse(List(1, 2, 3))(x => Option(x))
+
   import cats.syntax.traverse._
-  val optionList_v2:Option[List[Int]] = List(1,2,3).traverse(Option(_))
+
+  val optionList_v2: Option[List[Int]] = List(1, 2, 3).traverse(Option(_))
 
   println(optionList_v2)
 
@@ -95,6 +103,7 @@ object CatsIntro extends App {
     }
 
   def putStrLn(value: String) = IO(println(value))
+
   val readLn = IO(scala.io.StdIn.readLine())
   for {
     _ <- putStrLn("What's your name?")
---
+++ scala-at-light-speed/src/main/scala/com/rockthejvm/udemy/catts/IOIntroduction.scala
@@ -0,0 +1,115 @@
+package com.rockthejvm.udemy.catts
+
+import cats.effect.IO
+
+import scala.io.StdIn
+
+object IOIntroduction {
+
+  val firstIO: IO[Int] = IO.pure(42) //no side effects
+  val delayedIO: IO[Int] = IO.delay {
+    println("medved")
+    52
+  }
+
+  val delayedIO2: IO[Int] = IO { // apply == delay
+    println("medved2")
+    62
+  }
+
+  def smallProg(): IO[Unit] = for {
+   line1 <- IO(StdIn.readLine())
+   line2 <- IO(StdIn.readLine())
+    _ <- IO.delay(println(line1 + line2))
+  } yield ()
+
+  //mapN combine effects as tuples
+  import cats.syntax.apply._
+  val combined: IO[Int] = (firstIO, delayedIO).mapN(_ + _)
+
+  def smallProg2(): IO[Unit] = (IO(StdIn.readLine()),IO(StdIn.readLine())).mapN(_ + _).map(println)
+
+  //Exercises
+  //1
+  def seqAndTakeLast[A,B](ioa: IO[A], iob: IO[B]): IO[B] = for {
+    _ <- ioa
+    iobRes <- iob
+  } yield iobRes
+  def seqAndTakeLast_v2[A,B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa.flatMap(_ => iob)
+  def seqAndTakeLast_v3[A,B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa *> iob //andThen operator
+  def seqAndTakeLast_v4[A,B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa >> iob //andThen wtih by-name call
+
+  def seqAndTakeFirst[A,B](ioa: IO[A], iob: IO[B]): IO[A] = for {
+    ioaRes <- ioa
+    _ <- iob
+  } yield ioaRes
+  def seqAndTakeFirst_v2[A,B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa.flatMap(aRes => iob.map(_ => aRes))
+
+  def seqAndTakeFirst_v3[A,B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa <* iob
+
+  //3
+  import cats.effect.unsafe.implicits.global
+  def forever[A](io: IO[A]): IO[A] =
+    io.flatMap(_ => forever(io))
+//    io.flatMap(_ => {
+//      val newIo = IO(io.unsafeRunSync())
+//      forever(newIo)
+//    })
+  def forever2[A](io: IO[A]): IO[A] =
+    io >> forever2(io)
+
+  def forever3[A](io: IO[A]): IO[A] =
+    io *> forever3(io)
+
+  def forever4[A](io: IO[A]): IO[A] =
+    io.foreverM
+
+  //4
+  def convert[A,B](ioa: IO[A], value: B): IO[B] =
+    ioa.map(_ => value)
+  def convert2[A,B](ioa: IO[A], value: B): IO[B] =
+    ioa.as(value)
+
+  //5
+  def asUnit[A](ioa: IO[A]): IO[Unit] =
+    ioa.map(_ => ())
+  def asUnit2[A](ioa: IO[A]): IO[Unit] =
+    ioa.as(())//dont use this - not readable
+  def asUnit3[A](ioa: IO[A]): IO[Unit] =
+    ioa.void
+
+  //6 - fix recursion
+  def sum(n: Int): Int =
+    if (n <= 0) 0
+    else n + sum(n - 1)
+
+  def sumIO(n:Int): IO[Int] =
+    if (n <= 0) IO(0)
+    else sumIO(n -1).map(res => res + n)
+
+
+  //7 write fibbonacchi
+  def fibo(n:Int): IO[BigInt] = {
+    if (n < 2) IO.Pure(1)
+    else IO.pure(1)
+  }
+
+  def main(args:Array[String]): Unit = {
+    import cats.effect.unsafe.implicits.global
+//    println(firstIO.unsafeRunSync())
+//    println(delayedIO.unsafeRunSync())
+//    println(combined.unsafeRunSync())
+//    println(smallProg().unsafeRunSync())
+
+    println(seqAndTakeLast(delayedIO, delayedIO2).unsafeRunSync())
+    println(seqAndTakeFirst(delayedIO, delayedIO2).unsafeRunSync())
+//    forever2(delayedIO).unsafeRunSync()
+
+    println(sumIO(5).unsafeRunSync())
+  }
+}
