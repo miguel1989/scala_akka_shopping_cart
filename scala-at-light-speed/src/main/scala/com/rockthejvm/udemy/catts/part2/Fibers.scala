518c69437e56a7b6996508e3e0eed791578bc52a
mihailsk <mkrasikovs@onairent.live>
2021-12-06 11:44:11 +0200

    training materials

--- scala-at-light-speed/src/main/scala/com/rockthejvm/udemy/catts/part2/Fibers.scala
+++ scala-at-light-speed/src/main/scala/com/rockthejvm/udemy/catts/part2/Fibers.scala
@@ -1,8 +1,11 @@
 package com.rockthejvm.udemy.catts.part2
 
 import cats.effect.kernel.Outcome
+import cats.effect.kernel.Outcome.{Canceled, Errored, Succeeded}
 import cats.effect.{Fiber, FiberIO, IO, IOApp, ParallelF}
 import com.rockthejvm.udemy.catts.DebugWrapper
+
+import scala.concurrent.duration.FiniteDuration
 
 object Fibers extends IOApp.Simple {
 
@@ -30,6 +33,49 @@ object Fibers extends IOApp.Simple {
     fib <- IO.raiseError[Int](new RuntimeException("medved err")).start
     result <- fib.join
   } yield result
+
+  def processResultsFromFiber[A](io: IO[A]): IO[A] = {
+    val result = for {
+      fib <- io.debugCustom.start
+      result <- fib.join //an effect which waits for the fiber to terminate
+    } yield result
+
+    result flatMap {
+      case Succeeded(fa) => fa
+      case Errored(e) => IO.raiseError(e)
+      case Canceled() => IO.raiseError(new RuntimeException("aaa"))
+    }
+  }
+
+  def tupleIOS[A,B](ioa: IO[A], iob: IO[B]): IO[(A,B)] = {
+    val result = for {
+      fib1 <- ioa.debugCustom.start
+      fib2 <- iob.debugCustom.start
+      result1 <- fib1.join //an effect which waits for the fiber to terminate
+      result2 <- fib2.join //an effect which waits for the fiber to terminate
+    } yield (result1, result2)
+
+    result.flatMap {
+      case (Succeeded(fa), Succeeded(fb)) => for {
+        a <- fa
+          b<- fb
+      } yield (a, b)
+      case _ => IO.raiseError(new RuntimeException("cancelled"))
+    }
+  }
+
+  def timeout[A](io: IO[A], duration: FiniteDuration): IO[A] = {
+    val computation = for {
+      fib <- io.start
+      _ <- (IO.sleep(duration) >> fib.cancel).start //careful - can leak
+      result <- fib.join
+    } yield result
+
+    computation flatMap {
+      case Succeeded(fa) => fa
+      case _ => IO.raiseError(new RuntimeException("aaa"))
+    }
+  }
 
   override def run: IO[Unit] = runOnSomeOtherThread(meaning).debugCustom.void
 }
